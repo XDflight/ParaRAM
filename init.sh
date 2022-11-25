c306db33edc38deed16b621a9c67fa48df8664f1
Qirui Da <ridge0802@126.com>
2022-11-25 20:41:39 +0800

    Update files.

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 export OPENLANE_ROOT=$(pwd)/dependencies/openlane_src
 export PDK_ROOT=$(pwd)/dependencies/pdks
+export PRECHECK_ROOT=/Volumes/Efabless/mpw_precheck
 export PDK=gf180mcuC
 export DOCKER_SWAP=-1
